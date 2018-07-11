c9f537cfe83fe92639723677fe1d71b71a7fc761
Nathan Lin <nlin@theticker.org>
2018-07-11 20:49:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,10 +14,10 @@ function yourAnimal() {
 }
 
 function add2(n) {
-  return n + two
+  return (n + two)
 
   // Feel free to move things around!
-  const two = 2
+  var two = 2
 }
 
 var funkyFunction = function() {
